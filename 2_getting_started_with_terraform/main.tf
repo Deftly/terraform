f8c738cbc4cc0bfcd0a517bfad22667603cda89e
Deftly <deftkz@gmail.com>
2022-07-21 20:21:25 -0700

    Getting started with Terraform initial chapter exercises

--- 2_getting_started_with_terraform/main.tf
+++ 2_getting_started_with_terraform/main.tf
@@ -3,14 +3,14 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami                    = "ami-01f87c43e618bf8f0"
-  instance_type          = "t2.micro"
+  ami           = "ami-085284d24fe829cd0"
+  instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.instance.id]
 
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
-              nohup busybox httpd -f -p ${var.server_port} &
+              nohup busybox httpd -f -p 8080 &
               EOF
 
   tags = {
@@ -21,11 +21,10 @@ resource "aws_instance" "example" {
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
-  ingress {
-    from_port   = var.server_port
-    to_port     = var.server_port
+  ingress = {
+    from_port   = 8080
+    to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
 }
--- terraform_up_and_running/2_getting_started_with_terraform/webserver_cluster/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "server_port" {
-  description = "The port server will use for HTTP requests"
-  type        = number
-  default     = 8080
-}
